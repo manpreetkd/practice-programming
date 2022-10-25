e77d2478f135ea2c66f12fba5032a2fb91a3be8d
Manpreet K Dhanjal <manpreetkdhanjal@Manpreets-MacBook-Pro.local>
2022-10-25 15:14:32 +0530

    Results reflect floor and integer division functionality

---
+++ python-loops/nonnegative.py
@@ -0,0 +1,10 @@
+'''
+Task
+The provided code stub reads and integer, n, from STDIN. 
+For all non-negative integers i < n, print i^2.
+'''
+
+if __name__ == '__main__':
+    i = int(input())
+    for n in range(i):
+        print(n**2)
